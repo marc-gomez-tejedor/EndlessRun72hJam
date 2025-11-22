e4439591fb3c52654ba177de061b0c5e9d454cbb
Marc <marcgomeztejedor@gmail.com>
2025-11-22 21:17:41 +0100

    random canvas painter

--- Assets/Scripts/TestSpawner.cs
+++ Assets/Scripts/TestSpawner.cs
@@ -2,22 +2,29 @@ using UnityEngine;
 
 public class TestSpawner : MonoBehaviour
 {
-    [SerializeField]
     GameObject _objectToSpawn;
     float time;
 
+    [SerializeField]
+    GameObject[] objectsToChoose;
+
     private void Awake()
     {
+        _objectToSpawn = objectsToChoose[0];
         time = 0f;
     }
     private void Update()
     {
         time += Time.deltaTime;
-        float t = Mathf.Cos(time);
+        float t = Mathf.Cos(0.25f*time);
         if (Input.GetMouseButton(0))
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             Instantiate(_objectToSpawn, ray.origin+ray.direction*(15f+t), Quaternion.identity);
         }
+    }
+    public void SwapSpawnedObj(int id)
+    {
+        _objectToSpawn = objectsToChoose[id];
     }
 }
